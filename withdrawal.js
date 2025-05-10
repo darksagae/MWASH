837b83c72e638a9af33177a624f4ab64c0f2cca3
darksagae <darksagaedomain@gmail.com>
2025-05-10 05:39:19 -0400

    Update withdrawal.js

--- withdrawal.js
+++ withdrawal.js
@@ -43,6 +43,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // Form submission with pop-up
     document.getElementById('withdrawal-form').addEventListener('submit', (e) => {
         e.preventDefault();
-        alert('Dear pioneer\n Our recent system upgrade is now complete and we're working swiftly to restore full service.\n Thank you for understanding and patience during this brief transition.');
+        alert('Dear pioneer\n Our recent system upgrade is now complete and we're working swiftly to restore full service.\n Thank you fõr understanding and patience during this brief transition.');
     });
 });
